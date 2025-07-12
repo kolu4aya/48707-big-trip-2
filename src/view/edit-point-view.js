ac981553845b4cac443305379f6708a861f8022e
Марченко Виктория <m.n.i.mo@mail.ru>
2025-07-12 21:35:02 +0600

    correct singlequote

--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -1,15 +1,15 @@
-import AbstractStatefulView from "../framework/view/abstract-stateful-view.js";
-import { TYPE_OF_ROUTE } from "../const";
-import { getDestination } from "../mock/destination";
-import { getOffers } from "../mock/offers";
-import { humanizeTaskDueDate } from "../utils/point";
+import AbstractStatefulView from '../framework/view/abstract-stateful-view.js';
+import { TYPE_OF_ROUTE } from '../const';
+import { getDestination } from '../mock/destination';
+import { getOffers } from '../mock/offers';
+import { humanizeTaskDueDate } from '../utils/point';
 
 const BLANK_POINT = {
   type: TYPE_OF_ROUTE[5],
-  destination: "",
-  offers: "",
-  timeStart: "",
-  timeEnd: "",
+  destination: '',
+  offers: '',
+  timeStart: '',
+  timeEnd: '',
   favorite: false,
   cost: 0,
 };
@@ -21,24 +21,24 @@ const createEditPointTemplate = (data) => {
     items
       .map((item) => {
         const typeLow = item.toLowerCase();
-        const checked = item === type ? " checked" : "";
+        const checked = item === type ? ' checked' : '';
         return `<div class='event__type-item'>
               <input id='event-type-${typeLow}-1' class='event__type-input  visually-hidden' type='radio' name='event-type' value='${item}' ${checked}>
               <label class='event__type-label  event__type-label--${typeLow}' for='event-type-${typeLow}-1'>${item}</label>
             </div>`;
       })
-      .join("");
+      .join('');
 
   const destinationsToString = (items = []) =>
-    items.map((item) => `<option value='${item.name}'></option>`).join("");
+    items.map((item) => `<option value='${item.name}'></option>`).join('');
 
   const offersToString = (items = [], typeForOffer) => {
     items = items.filter((item) => item.type === typeForOffer);
 
     return items
       .map((item) => {
-        const itemAtr = item.title.split(item.title.split(" ").length - 1);
-        const checked = offers.includes(item) ? " checked" : "";
+        const itemAtr = item.title.split(item.title.split(' ').length - 1);
+        const checked = offers.includes(item) ? ' checked' : '';
         return `<div class='event__offer-selector'>
                 <input class='event__offer-checkbox  visually-hidden' id='event-offer-${itemAtr}-1' type='checkbox' name='event-offer-${itemAtr}' ${checked}>
                 <label class='event__offer-label' for='event-offer-${itemAtr}-1'>
@@ -48,7 +48,7 @@ const createEditPointTemplate = (data) => {
                 </label>
               </div>`;
       })
-      .join("");
+      .join('');
   };
 
   const photoToString = (destinationForPhoto) => {
@@ -59,8 +59,8 @@ const createEditPointTemplate = (data) => {
     );
   };
 
-  const dateStart = humanizeTaskDueDate(timeStart, "DD/MM/YYYY HH:mm");
-  const dateEnd = humanizeTaskDueDate(timeEnd, "DD/MM/YYYY HH:mm");
+  const dateStart = humanizeTaskDueDate(timeStart, 'DD/MM/YYYY HH:mm');
+  const dateEnd = humanizeTaskDueDate(timeEnd, 'DD/MM/YYYY HH:mm');
 
   console.log(destination);
 
@@ -85,9 +85,7 @@ const createEditPointTemplate = (data) => {
                     <label class='event__label  event__type-output' for='event-destination-1'>
                       ${type}
                     </label>
-                    <input class='event__input  event__input--destination' id='event-destination-1' type='text' name='event-destination' required value='${
-                      destination !== undefined ? destination.name : ""
-                    }' list='destination-list-1'>
+                    <input class='event__input  event__input--destination' id='event-destination-1' type='text' name='event-destination' required value='${destination !== undefined ? destination.name : ''}' list='destination-list-1'>
                     <datalist id='destination-list-1'>
                       ${destinationsToString(getDestination())}
                     </datalist>
@@ -127,23 +125,16 @@ const createEditPointTemplate = (data) => {
                   <section class='event__section  event__section--destination'>
                     <h3 class='event__section-title  event__section-title--destination'>Destination</h3>
                     <p class='event__destination-description'>
-                    ${
-                      destination !== undefined ? destination.description : ""
-                    }</p>
+                    ${destination !== undefined ? destination.description : ''}</p>
 
                     <div class='event__photos-container'>
                       <div class='event__photos-tape'>
-                       ${
-                         destination !== undefined
-                           ? photoToString(destination)
-                           : ""
-                       }
+                       ${destination !== undefined ? photoToString(destination): ''}
                       </div>
                     </div>
                   </section>
                 </section>
-              </form>
-              `;
+              </form>`;
 };
 
 export default class EditPointView extends AbstractStatefulView {
@@ -162,18 +153,18 @@ export default class EditPointView extends AbstractStatefulView {
   }
 
   _restoreHandlers() {
-    this.element.addEventListener("submit", this.#formSubmitHandler);
+    this.element.addEventListener('submit', this.#formSubmitHandler);
     this.element
-      .querySelector(".event--edit .event__rollup-btn")
-      .addEventListener("click", this.#formSubmitHandler);
+      .querySelector('.event--edit .event__rollup-btn')
+      .addEventListener('click', this.#formSubmitHandler);
     this.element
-      .querySelectorAll(".event__type-input")
+      .querySelectorAll('.event__type-input')
       .forEach((item) =>
-        item.addEventListener("change", this.#typeChangeHandler)
+        item.addEventListener('change', this.#typeChangeHandler)
       );
     this.element
-      .querySelector(".event__input--destination")
-      .addEventListener("change", this.#destinationChangeHandler);
+      .querySelector('.event__input--destination')
+      .addEventListener('change', this.#destinationChangeHandler);
   }
 
   #formSubmitHandler = (evt) => {
