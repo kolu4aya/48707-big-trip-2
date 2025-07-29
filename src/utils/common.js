ac0cb5d4f4c129ea9481b4e0f975856c4e34232a
Марченко Виктория <m.n.i.mo@mail.ru>
2025-07-29 22:54:42 +0600

    correct

--- src/utils/common.js
+++ src/utils/common.js
@@ -7,13 +7,3 @@ export const generatingAuthString = () => {
 
   return encodedCredentials;
 }
-
-export const generatingAuthString = () => {
-  const login = 'kolu4aya';
-  const password = '*Z@WyPXpz1Kx';
-
-  const credentials = `${login}:${password}`;
-  const encodedCredentials = btoa(credentials);
-
-  return encodedCredentials;
-};
